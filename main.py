bc7f23a07874fe305ce9abf885cfea92cde2cc45
Ajay Thakur <ajaythk.94@gmail.com>
2024-09-08 19:50:38 +0545

    requirements_changed

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": " Hello World"}
+
+
+@app.get('/greet/{name}/')
+def greet_name(name: str) -> dict:
+    """
+    If we remove {name} from @app.get('/greet/{name}') then append url {name} with query params.
+    :param name:
+    :return: message
+    """
+    return {"message": f"Hello {name}"}
