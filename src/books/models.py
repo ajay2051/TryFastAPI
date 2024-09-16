e568316febc580ba2956d0a8858426bbd05c1f7a
Ajay Thakur <ajaythk.94@gmail.com>
2024-09-16 20:56:49 +0545

    columns added

--- src/books/models.py
+++ src/books/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 
 from app.db_connection import Base
 
@@ -8,3 +8,12 @@ class Books(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     author = Column(String)
+    publisher = Column(String)
+    published_date = Column(String)
+    page_count = Column(Integer)
+    language = Column(String)
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
+
+    def __repr__(self):
+        return f"Book {self.title} {self.author}"
